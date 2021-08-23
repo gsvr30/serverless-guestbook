8fc8392fc81ef5003928c9ed5f3d8285b3a6afff
gsvr30 <36774910+gsvr30@users.noreply.github.com>
2021-08-23 16:58:06 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b8a5645b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
